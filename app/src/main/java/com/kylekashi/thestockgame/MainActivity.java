77c0d7016a53db28bc43da6e021e14098b77782a
Marco Auer <marcoa91@gmx.at>
2018-01-24 12:15:29 +0100

    Add some basic elements to main

--- app/src/main/java/com/kylekashi/thestockgame/MainActivity.java
+++ app/src/main/java/com/kylekashi/thestockgame/MainActivity.java
@@ -2,12 +2,28 @@ package com.kylekashi.thestockgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> stocks;
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        stocks = new ArrayList<>(asList("1","2","3","4","5","6","7"));
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stocks);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
